61f9deaa9376de4bbccd5d4889fc5d2d5f2472f5
Elias Ojala <git@eliasojala.me>
2018-05-29 22:20:29 +0300

    div.kouluruoka-test1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+/**
+ * ArrayConstructor with .from()
+ * https://stackoverflow.com/a/36719911/6451184
+ */
+interface ArrayConstructor {
+  from(arrayLike: any, mapFn?, thisArg?): Array<any>;
+}
+
+/**
+ * Collection of `a.kouluruoka-test1`
+ */
+const test1Elements = document.getElementsByClassName("kouluruoka-test1");
+
+Array.from(test1Elements).forEach(element => {
+  element.innerHTML = "foo";
+});
