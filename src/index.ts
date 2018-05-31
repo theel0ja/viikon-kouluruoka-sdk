640fd2984b4458e1e0525cdec18b8391b5137c6b
Elias Ojala <git@eliasojala.me>
2018-05-31 18:25:48 +0300

    Polyfilled Array.from

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-/**
- * ArrayConstructor with .from()
- * https://stackoverflow.com/a/36719911/6451184
- */
-interface ArrayConstructor {
-  from(arrayLike: any, mapFn?: any, thisArg?: any): Array<any>;
-}
+import "core-js/es6/array"; // IE support (https://stackoverflow.com/a/41907111/6451184)
 
 /**
  * Collection of `a.kouluruoka-test1`
  */
 const test1Elements = document.getElementsByClassName("kouluruoka-test1");
 
-Array.from(test1Elements).forEach(element => {
+Array.from(test1Elements).forEach((element) => {
   element.innerHTML = "foo";
 });
