2e09e43156109b27d98706552ed4e6e77a142452
Elias Ojala <git@eliasojala.me>
2018-06-08 14:25:48 +0300

    Working copy

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,63 @@
 import "core-js/es6/array"; // IE support (https://stackoverflow.com/a/41907111/6451184)
 
-function createIframe(source: string): HTMLIFrameElement {
+const settings = {
+  defaultHeight: 480, // as pixels
+  defaultWidth: 640, // as pixels
+};
+
+function createIframe(source: string, width?: number | string, height?: number | string): HTMLIFrameElement {
   const element = document.createElement("iframe");
 
-  // Disable borders
+  // Disable borders and set max width to 100%
   element.frameBorder = "0";
+  element.style.maxWidth = "100%";
 
-  element.src = "https://www.example.com"; // source;
+  // Set src
+  element.src = source;
 
+  // Set width and height
+  if (width) {
+    element.width = width.toString();
+  } else {
+    element.width = settings.defaultWidth.toString();
+  }
+
+  if (height) {
+    element.height = height.toString();
+  } else {
+    element.height = settings.defaultHeight.toString();
+  }
+
+  // Return the element
   return element;
 }
 
 /**
- * Collection of `a.kouluruoka-test1`
+ * Collection of `a.kouluruoka-menu`
  */
-const test1Elements = document.querySelectorAll("div[data-src].kouluruoka-test1");
+const menuElements = document.querySelectorAll("div[data-src].kouluruoka-menu");
 
 // FIXME: Try to remove this `Array as any`
-(Array as any).from(test1Elements).forEach((element: HTMLDivElement) => {
+(Array as any).from(menuElements).forEach((element: HTMLDivElement) => {
 
-  // URL in <a>
-  const sourceUrl = element.dataset.src;
+  // URL in <a> and remove query parameters and fragment
+  let sourceUrl = element.dataset.src;
+  sourceUrl = sourceUrl.split("#")[0];
+  sourceUrl = sourceUrl.split("?")[0];
 
   // Parse sourceUrl
-  const iframeUrl = sourceUrl;
+  let iframeUrl = sourceUrl;
+  iframeUrl = iframeUrl + "/embed"; // Add /embed suffix
+
+  // Set width and height (TODO:)
+
+  // tslint:disable-next-line:prefer-const
+  let width;
+  // tslint:disable-next-line:prefer-const
+  let height;
 
   // Create iframe
-  const iframe = createIframe(iframeUrl);
+  const iframe = createIframe(iframeUrl, width, height);
 
   // Remove stuff inside the element
   element.innerText = "";
