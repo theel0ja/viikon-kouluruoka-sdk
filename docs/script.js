972fac1c821f967175cf2f0e744822123735b031
Elias Ojala <git@eliasojala.me>
2018-09-17 16:42:24 +0300

    LibreJS to docs/script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 function listEndpoints(endpoints) {
   const availableEndpointsEl = document.getElementById("availableEndpoints");
   
@@ -15,5 +16,5 @@ function listEndpoints(endpoints) {
     
     availableEndpointsEl.append(endpointEl);
   });
-
 }
+// @license-end
