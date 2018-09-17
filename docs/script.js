8af8beb1a88fe6c57bec3f8f5d9c9a8b6ca2002e
Elias Ojala <git@eliasojala.me>
2018-09-17 15:18:47 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,17 @@
-function listEndpoints(data) {
-  const element = document.getElementById("availableEndpoints");
+function listEndpoints(endpoints) {
+  const availableEndpointsEl = document.getElementById("availableEndpoints");
+  
+  endpoints.forEach(function(endpoint) {
+    const endpointEl = document.createElement("li"); // ul#availableElement -> li
+    
+    
+    const endpointPre = document.createElement("pre"); // ul#availableElement -> li -> pre
+    endpointPre.innerHTML = endpoint.api_urL;
+    endpointEl.append(endpointPre);
+    
+    
+    availableEndpointsEl.append(endpointEl);
+  });
   
   console.log(data);
 }
